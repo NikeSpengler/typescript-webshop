13ab01378df97d2a8eedb9c7d738128ab0ad09e5
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-05-15 12:39:40 +0200

    Navigation update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Navbar as NavbarBs } from "react-bootstrap"
+import { Container } from "react-bootstrap"
+
+export function Navbar() {
+    return (
+        <NavbarBs>
+            <Container>
+                Nav
+            </Container>
+        </NavbarBs>
+    ) 
+}
