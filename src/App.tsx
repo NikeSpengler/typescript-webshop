8cdbc658d7670f53a4568bb5eef3085b3017024c
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-05-15 12:18:48 +0200

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-
+import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
 
 function App() {
 
 
   return (
-    <>
-     <h1>Hi</h1>
-    </>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Store/>}/>
+        <Route path="/" element={<About/>}/>
+      </Routes>
+    </Container>
   )
 }
 
